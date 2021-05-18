1967416c8b6ee46c8930f8645646119a6c156f7f
thuyanh.vuong <thuyanh.vuong@mission.plus>
2021-05-18 20:06:03 +0700

    commit

--- scripts/metadata.js
+++ scripts/metadata.js
@@ -1,22 +1,34 @@
 module.exports = {
   TS19h_0830: {
     url: "https://youtu.be/fRbc0I_YU2Q",
+    source:
+      "https://vod2.hanoitv.vn/CLIP2020/Thang08/FM90/Thoi_su_Ha_noi/30_8%20ts%2019h.mp3",
     thumbnail: "http://i3.ytimg.com/vi/fRbc0I_YU2Q/maxresdefault.jpg",
+    category: ["TS", "19h"],
+    duration: 1857,
   },
   TS13h_0409: {
-    url: "https://youtu.be/03Bulbwds1E",
+    url: "https://media.sparemin.com/embed-video/videos/07/82/c0/29/0782c029-cf38-40cd-8b69-4c8d43378c8e_4563950.mp4",
     thumbnail: "http://i3.ytimg.com/vi/03Bulbwds1E/maxresdefault.jpg",
+    category: ["TS", "13h"],
+    duration: 1737,
   },
   TYHN_2608: {
-    url: "https://youtu.be/4ave2eHUlhY",
+    url: "https://media.sparemin.com/embed-video/videos/bf/30/e7/7c/bf30e77c-53d2-4682-b27b-039cbce9b582_4593391.mp4",
     thumbnail: "http://i3.ytimg.com/vi/4ave2eHUlhY/maxresdefault.jpg",
+    category: ["TYHN"],
+    duration: 3576,
   },
   TGM_0309: {
-    url: "https://youtu.be/qEBPEehV_iU",
+    url: "https://media.sparemin.com/embed-video/videos/0d/8b/77/50/0d8b7750-8e3e-4387-a882-27049d9e7537_4561589.mp4",
     thumbnail: "http://i3.ytimg.com/vi/qEBPEehV_iU/maxresdefault.jpg",
+    category: ["TS"],
+    duration: 1865,
   },
   TS19h_0409: {
     url: "https://media.sparemin.com/embed-video/videos/d0/57/51/c1/d05751c1-115c-468e-b965-e6201e2e73df_4799407.mp4",
     thumbnail: "http://i3.ytimg.com/vi/qEBPEehV_iU/maxresdefault.jpg",
+    category: ["TS", "19h"],
+    duration: 1904,
   },
 };
